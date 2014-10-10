2b268eb603d867d3aae97e78d73c4c0832389a95
Zachary Skalko <zskalko@gmail.com>
2014-10-09 21:54:17 -0500

    Add code from previous project
    Add a bunch of stuff from the previous project. Most notable - the api
    service starting on line 169 of hcardAngular.js

---
+++ hcardAngular.js
@@ -0,0 +1,241 @@
+var app = angular.module('honorCard', ['ngRoute','ngCookies']);
+
+app.directive('focusOn', function() {
+  return {
+    link: function(scope, element, attrs) {
+      scope.$watch(attrs.focusOn, function(value) {
+        if(value === true) { 
+ 
+            element[0].focus();
+
+        }
+      });
+    }
+  };
+});
+
+app.config(['$routeProvider',function($routeProvider){
+
+  $routeProvider
+    .when('/login', {
+      templateUrl : "templates/login.html",
+      controller :  "loginCtrl"  
+    })
+  $routeProvider
+    .when('/lobby', {
+      templateUrl : "templates/lobby.html",
+      controller :  "lobbyCtrl"  
+    })
+  $routeProvider
+    .when('/board', {
+      templateUrl : "templates/board.html",
+      controller :  "boardCtrl"  
+    })
+  $routeProvider
+    .when('/deck', {
+      templateUrl : "templates/deck.html",
+      controller :  "deckCtrl"  
+    })
+  $routeProvider
+    .when('/stats', {
+      templateUrl : "templates/stats.html",
+      controller :  "statsCtrl"  
+    })
+	.otherwise({ redirectTo: '/sales'})
+
+}]);
+
+app.run(function($rootScope, $timeout, $interval, $location, $cookies,api,$window) {
+
+
+  $rootScope.isLoggedIn = function(){
+    return (angular.isDefined($cookies.apitoken));
+  }
+  
+  $rootScope.location = function(){
+    return $location.path();
+  
+  }
+
+  $rootScope.$watch($rootScope.isLoggedIn, function(value){
+  
+    if(value){
+
+    if($location.path() == "/login"){
+        $location.path('/lobby');
+
+      }
+
+		}else{
+    
+    $location.path('/login');
+    
+    }
+
+    
+  
+  });
+  
+
+  
+  
+  $rootScope.redirectToLogin = function(){
+    if(!$rootScope.isLoggedIn()){
+      $location.path('/login');
+    }
+  }
+  
+  $rootScope.redirectToLogin();
+  
+  $rootScope.$watch($rootScope.location, function(value){
+    
+    $rootScope.redirectToLogin();
+  
+  });
+  
+
+  
+
+
+  
+  $rootScope.$watch(function(){
+       return $window.innerWidth;
+    }, function(value) {
+    
+    if(value <= 440){
+       
+       $rootScope.isMobile = true;
+       
+       }
+       else{
+       
+       $rootScope.isMobile = false;
+       
+       }
+   });
+  
+
+
+});
+
+app.controller('headerCtrl', function($scope,$location,$rootScope,$cookies,api){
+
+  $scope.checkLocation = function(path){
+
+    return path === $location.path();
+
+  }
+
+  $scope.logout = function(){
+  
+    api.logout();
+  }
+
+});
+
+app.controller('loginCtrl', function($scope,api){
+
+	$scope.login = function(){
+		
+    api.login($scope.creds);
+    delete $scope.creds;
+	
+	}
+  
+
+});
+
+
+app.controller('lobbyCtrl', function(){
+
+});
+
+app.controller('boardCtrl', function(){
+
+});
+
+app.controller('deckCtrl', function(){
+
+});
+
+app.controller('statsCtrl', function(){
+
+});
+
+
+
+
+
+app.service('api', function($rootScope,$cookieStore,$http,$cookies){
+
+  this.login = function(credentials){
+    $rootScope.isLoggingIn = true;
+    delete $rootScope.loginError;
+    
+    
+    postLogin = function(credentials,callback){
+
+      $http({
+        url:'api/login.php',
+        method:'POST',
+        data: credentials
+      })
+      .success(function(data){
+        callback(data);
+
+      })
+      .error(function(data){
+        callback({failed:"Error with HTTP Service"});
+      });
+
+    }
+  
+    checkLogin = function(data){
+    
+      if(angular.isUndefined(data.failed)){
+        $cookieStore.put('apitoken', data);
+
+      }
+      else{
+        $rootScope.loginError = data.failed;
+        
+      }
+    $rootScope.isLoggingIn = false; 
+    }
+  
+  
+    postLogin(credentials,checkLogin);
+  
+  }
+
+  
+  this.logout = function(){
+
+    $cookieStore.remove('apitoken');
+    
+  }
+  
+  this.call = function(action,data,callback){
+
+    if(angular.isDefined($cookies.apitoken)){
+    $http({
+      url:'api/action.php',
+      method:'POST',
+      data: {"token":$cookies.apitoken,"action":action,"data":data}
+    })
+    .success(function(response){
+      callback(response,true);
+
+    })
+    .error(function(data){
+      callback(response,false);
+      console.log(response);
+    });
+    
+    
+    }
+  
+  
+  }
+
+});
---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html ng-app="honorCard">
+<head>
+  <title>Honor Card</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=440, user-scalable=no">
+  <link href='http://fonts.googleapis.com/css?family=Cinzel:400,700,900' rel='stylesheet' type='text/css'>
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.25/angular.min.js"></script>
+  <script src="http://cdnjs.cloudflare.com/ajax/libs/angular.js/1.2.20/angular-cookies.min.js"></script>
+  <script src="http://cdnjs.cloudflare.com/ajax/libs/angular.js/1.2.20/angular-route.js"></script>
+  <script src="hcardAngular.js"></script>
+  <link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body>
+<div ng-controller="headerCtrl">
+<header ng-show="isMobile">
+<h1>Honor Card</h1>
+</header>
+<header >
+	<h1 ng-hide="isMobile">Honor Card</h1>
+	<a ng-show="isLoggedIn()" href="#/lobby" ng-class="{active:checkLocation('/lobby')}">Lobby</a>
+	<a ng-show="isLoggedIn()" href="#/board" ng-class="{active:checkLocation('/board')}">Board</a>
+	<a ng-show="isLoggedIn()" href="#/deck" ng-class="{active:checkLocation('/deck')}">Deck</a>
+	<a ng-show="isLoggedIn()" href="#/stats" ng-class="{active:checkLocation('/stats')}">Stats</a>
+	<a ng-show="isLoggedIn()" ng-click="logout()" class="tail">Log Out</a>
+	<a ng-hide="isLoggedIn() || isMobile" class="active tail">Log In</a>
+
+	</header>
+</div>
+	
+<div id="container" ng-view>
+</div>
+</body>
+</html>
